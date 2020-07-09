b79c2d1e827d59c0606a3e9241501aa26a89a77f
tdhansen33 <hansen.tyler3@outlook.com>
2020-07-09 13:48:04 -0500

    Added Features 1 and 2

--- src/App.js
+++ src/App.js
@@ -12,11 +12,32 @@ export default class App extends Component{
 
     // To ensure that we have all the necessary features from React to create a stateful component, we need to call a method called super(). This super() calls the constructor for the Component class in React.
     super();
+
+    this.state = {
+      userName: "Tyler",
+      todoItems: [
+        {action: "Get Exalted with Voldunai", done: false},
+        {action: "Level Druid to Max", done: false},
+        {action: "Make 120,000 Gold", done: false},
+        {action: "Get good at Jhin", done: false},
+        {action: "Buy Brendan new headset", done: false}
+      ]
+    }
   }
 
   // When using fat arrow (lamda) syntax the return keyword is not needed and the curly braces (scope) around the body of the function is also not needed
   render = () =>
     <div>
-
+      <ToDoBanner
+        Name = {this.state.userName}
+        Tasks = {this.state.todoItems}
+      />
     </div>
+};
+
+export class ToDoBanner extends Component{
+  render = () =>
+    <h4 className="bg-primary text-white text-center p-2">
+      {this.props.Name}'s To Do List ({this.props.Tasks.filter(task => !task.done).length} items left)
+    </h4>
 };
